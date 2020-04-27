dae0924ade05e75b8bf3994dbab01b1f285beba8
designerofthing <designerofthings@gmail.com>
2020-04-27 16:25:26 +0200

    adds systemSelector to change inputs for impeial and metric, (wip)

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,24 +5,25 @@ const Form = props => {
       return (
         <form onSubmit={props.onSubmitHandler}>
           <input 
+            defaultChecked="metric"
             type="radio"
             id="metric"
             name="system"
             value="metric"
-            >
+            />
           <label htmlFor="metric">Metric</label>
           <input 
             type="radio"
             id="imperial"
             name="system"
             value="imperial"
-            >
-          <label htmlFor="imperial">Imperial</label>
+            />
+          <label htmlFor="imperial">Imperial</label><br />
           <label htmlFor="weight">Weight</label>
           <input
             type="number"
             required
-            placeholder="Weight in kgs"
+            placeholder={`Weight in ${"metric" == system.value ? "kgs":"pounds"}`}
             value={props.weight}
             name="weight"
             id="weight"
@@ -42,6 +43,45 @@ const Form = props => {
         </form>
       );
     };
+  
+    const systemSelector = system => {
+      if (system == "metric") {
+        return (
+        <>
+        <label htmlFor="height">Height</label>
+        <input
+        type="number"
+        required
+        placeholder="Height in cm"
+        value={props.height}
+        name="height"
+        id="height"
+        onChange={props.onChangeHandler}
+        /> </>
+        )} else {
+          return(
+          <>
+          <label htmlFor="feet">Height</label>
+          <input
+          type="number"
+          required
+          placeholder="Height in feet"
+          value={props.height}
+          name="feet"
+          id="feet"
+          onChange={props.onChangeHandler}
+          />
+          <input
+          type="number"
+          required
+          placeholder="inches"
+          value={props.height}
+          name="inches"
+          id="inches"
+          onChange={props.onChangeHandler}
+          /></>
+          )
+      }
+    }
 
     export default Form;
-  
